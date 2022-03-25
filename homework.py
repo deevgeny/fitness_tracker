a246238e0de71e525677006a46b8e205e5ce31e6
Evgeny Deriglazov <evgenyderiglazov@gmail.com>
2022-03-25 10:09:59 +0300

    3rd code review

--- homework.py
+++ homework.py
@@ -1,11 +1,18 @@
-from dataclasses import dataclass
-from typing import ClassVar, Dict
+from dataclasses import dataclass, asdict
+from typing import ClassVar, Dict, Type
 
 
 @dataclass
 class InfoMessage:
     """Training info message."""
 
+    INFO_STRING: ClassVar[str] = (
+        'Тип тренировки: {training_type}; '
+        'Длительность: {duration:.3f} ч.; '
+        'Дистанция: {distance:.3f} км; '
+        'Ср. скорость: {speed:.3f} км/ч; '
+        'Потрачено ккал: {calories:.3f}.'
+    )
     training_type: str
     duration: float
     distance: float
@@ -14,20 +21,16 @@ class InfoMessage:
 
     def get_message(self) -> str:
         """Return training info message using instance attributes."""
-        INFO_STRING = ('Тип тренировки: {training_type}; '
-                       'Длительность: {duration:.3f} ч.; '
-                       'Дистанция: {distance:.3f} км; '
-                       'Ср. скорость: {speed:.3f} км/ч; '
-                       'Потрачено ккал: {calories:.3f}.')
-        return INFO_STRING.format(**self.__dict__)
+        return self.INFO_STRING.format(**asdict(self))
 
 
 @dataclass
 class Training:
     """Training (parent class)."""
 
-    LEN_STEP: ClassVar = 0.65
-    M_IN_KM: ClassVar = 1000
+    LEN_STEP: ClassVar[float] = 0.65
+    M_IN_KM: ClassVar[float] = 1000
+    MINUTES_IN_HOUR: ClassVar[float] = 60
     action: int
     duration: float
     weight: float
@@ -38,8 +41,7 @@ class Training:
         Calculate distance using below formula:
         mean_speed = action * LEN_STEP / M_IN_KM.
         """
-        distance = self.action * self.LEN_STEP / self.M_IN_KM
-        return distance
+        return self.action * self.LEN_STEP / self.M_IN_KM
 
     def get_mean_speed(self) -> float:
         """Get mean speed.
@@ -71,6 +73,10 @@ class Training:
 class Running(Training):
     """Training: Running (child class)."""
 
+    COEFF_1: ClassVar[float] = 18
+    COEFF_2: ClassVar[float] = 20
+    WORKOUT_TYPE: ClassVar[str] = 'RUN'
+
     def get_spent_calories(self) -> float:
         """Get spent calories.
 
@@ -80,14 +86,11 @@ class Running(Training):
         calories = expr_1 * weight / M_IN_KM * duration_in_minutes.
         """
         # Prepare formula terms
-        coeff_1: int = 18
-        coeff_2: int = 20
         mean_speed = self.get_mean_speed()
-        minutes_in_hour = 60
-        duration_in_minutes = self.duration * minutes_in_hour
+        duration_in_minutes = self.duration * self.MINUTES_IN_HOUR
 
         # Calculate calories
-        expr_1 = (coeff_1 * mean_speed - coeff_2)
+        expr_1 = (self.COEFF_1 * mean_speed - self.COEFF_2)
         calories = expr_1 * self.weight / self.M_IN_KM * duration_in_minutes
         return calories
 
@@ -96,6 +99,10 @@ class Running(Training):
 class SportsWalking(Training):
     """Training: Sports walking (child class)."""
 
+    COEFF_1: ClassVar[float] = 0.029
+    COEFF_2: ClassVar[float] = 0.035
+    PWR: ClassVar[int] = 2
+    WORKOUT_TYPE: ClassVar[str] = 'WLK'
     height: int
 
     def get_spent_calories(self) -> float:
@@ -104,19 +111,17 @@ class SportsWalking(Training):
         Override parent class method.
         Calcualte spent calories using below formulas:
         expr_1 = (mean_speed ** 2 // height) * 0.029 * weight
-        calories = (0.035 * weight + expr_1) * duration_in_minutes.
+        expr_2 = expr_1 * 0.029 * weight
+        calories = (0.035 * weight + expr_2) * duration_in_minutes.
         """
         # Prepare formula terms
-        coeff_1: float = 0.029
-        coeff_2: float = 0.035
         mean_speed = self.get_mean_speed()
-        power: int = 2
-        minutes_in_hour = 60
-        duration_in_minutes = self.duration * minutes_in_hour
+        duration_in_minutes = self.duration * self.MINUTES_IN_HOUR
 
         # Calculate calories
-        expr_1 = (mean_speed ** power // self.height) * coeff_1 * self.weight
-        calories = (coeff_2 * self.weight + expr_1) * duration_in_minutes
+        expr_1 = (mean_speed ** self.PWR // self.height)
+        expr_2 = expr_1 * self.COEFF_1 * self.weight
+        calories = (self.COEFF_2 * self.weight + expr_2) * duration_in_minutes
         return calories
 
 
@@ -124,7 +129,10 @@ class SportsWalking(Training):
 class Swimming(Training):
     """Training: Swimming (child class)."""
 
-    LEN_STEP: ClassVar = 1.38
+    LEN_STEP: ClassVar[float] = 1.38
+    COEFF_1: ClassVar[float] = 1.1
+    COEFF_2: ClassVar[float] = 2
+    WORKOUT_TYPE: ClassVar[str] = 'SWM'
     length_pool: int
     count_pool: int
 
@@ -148,36 +156,61 @@ class Swimming(Training):
         calories = (mean_speed + 1.1) * 2 * weight.
         """
         # Prepare formula terms
-        coeff_1: float = 1.1
-        coeff_2: int = 2
         mean_speed = self.get_mean_speed()
 
-        # Calculate calories
-        calories = (mean_speed + coeff_1) * coeff_2 * self.weight
-        return calories
+        # Calculate calories and return result
+        return (mean_speed + self.COEFF_1) * self.COEFF_2 * self.weight
 
 
 def read_package(workout_type: str, data: list) -> Training:
-    """Read sensors data and return training class instance."""
-    class_dict: Dict[int, Training] = {
-        'SWM': Swimming, 'RUN': Running, 'WLK': SportsWalking
+    """Read sensors data and return training class instance.
+
+    After adding new training type class, training_types
+    dictionary should be updated accordingly to make
+    fintess tracker work correctly."""
+    # Option 1 (pytest passed)
+    # Create dictionary with available training types
+    training_types: Dict[str, Type(Training)] = {
+        Swimming.WORKOUT_TYPE: Swimming,
+        Running.WORKOUT_TYPE: Running,
+        SportsWalking.WORKOUT_TYPE: SportsWalking
     }
-    return class_dict[workout_type](*data)
+
+    # Option 2 (pytest passed)
+    # Create dictionary with available training types
+    # training_types: Dict[str, Type[Training]] = {
+    #   c.WORKOUT_TYPE: c for c in Training.__subclasses__()
+    # }
+
+    # Evaluate if workout_type argument exists in training_types
+    if workout_type not in training_types:
+        raise ValueError(
+            (f'Invalid workout_type argument value {repr(workout_type)}. '
+             'Valid values: '
+             f'{", ".join([repr(i) for i in training_types])}.')
+        )
+    return training_types[workout_type](*data)
 
 
 def main(training: Training) -> None:
-    """Main function."""
+    """Main function.
+
+    Prints training info message.
+    """
+    # Get InfoMessage class instance from Training class instance
     info = training.show_training_info()
+    # Print info message
     print(info.get_message())
 
 
 if __name__ == '__main__':
+    # Create sensor training data package
     packages = [
         ('SWM', [720, 1, 80, 25, 40]),
         ('RUN', [15000, 1, 75]),
         ('WLK', [9000, 1, 75, 180]),
     ]
-
+    # Run fitness tracker for different training data packages
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
